5626a6fb77197382eaf1374365100ae9b978f180
kyle smith <chessmonger2112@yahoo.com>
2016-02-16 01:13:51 -0800

    got this amazing font size changer to work, also color background transitions then stops after being clicked.

--- main.js
+++ main.js
@@ -1,34 +1,91 @@
 $(function(){
   console.log("test");
+  var backgroundColor = "#DDD"
   var borderRadius = 0;
   var theta = 0;
-  var colorLimit = 16777217 //256 ^ 3;
-  var colorValue = 0;
-  var red = null;
-  var green = null;
-  var blue = null;
-  var hex = null;
 
-  var colorTimer = setInterval(function(){
-    hex = colorValue.toString(16);
-    var hexLength = hex.length;
-    for (var i = 0; i < 6 - hexLength;i ++)
-    {
-      hex = "0" + hex;
+  app = {};
+
+  var Model = Backbone.Model.extend({
+    defaults: {
+      sequence : []
     }
-    red = parseInt(hex.slice(0,2),16);
-    green = parseInt(hex.slice(2,4),16);
-    blue = parseInt(hex.slice(4,6),16);
-    console.log("color value is ",colorValue, " hex is ",hex,hex.length);
-    $("#header").css("background-color","rgb(" + red + "," + green + ","+ blue + ")");
-    colorValue +=4;
-    colorValue %= colorLimit;
-  },15);
+  });
+
+  player = new Model;
+
+  app.AppView = Backbone.View.extend({
+    el: '#container',
+    initialize: function () {
+    },
+    events: {
+     "click .color" : "color"
+    },
+    green:function(event){
+      console.log("green was clicked ",event);
+      eventG = event;
+    },
+    color: function(event){
+      var colorPressed = event.target.id
+      console.log("The color pressed is ", colorPressed);
+      player.attributes.sequence.push("green");
+      this.blink(colorPressed);
+    },
+    blink: function(color){
+      var colorLimit = 16777217 //256 ^ 3;
+      var colorValue = 0;
+      var colorTimer = setInterval(function(){
+        var hex = colorValue.toString(16);
+        var hexLength = hex.length;
+        for (var i = 0; i < 6 - hexLength;i ++)
+        {
+          hex = "0" + hex;
+        }
+        var red = parseInt(hex.slice(0,2),16);
+        var green = parseInt(hex.slice(2,4),16);
+        var blue = parseInt(hex.slice(4,6),16);
+        $("#" + color).css("background-color","rgb(" + red + "," + green + ","+ blue + ")");
+        colorValue +=4;
+        colorValue %= colorLimit;
+      },15);
+
+      setTimeout(timeOut, 1000);
+
+      function timeOut()
+      {
+        console.log("Time out was called!!");
+        clearInterval(colorTimer)
+        $("#" + color).css("background-color", "#DDD");
+      }
+    }
+  });
+
+
+app.appView = new app.AppView();
+
+
 
  // var buttonTimer = setInterval(function(){
  //    borderRadius = Math.round(Math.abs(Math.sin(theta) * 50));
  //    theta+= .01;
  //    $("button").css("border-radius",borderRadius + "%")
  // },15);
+var fontSize = 32;
+var deltaFont = .25;
+var fontTimer = setInterval(function(){
+  if (fontSize > 50)
+  {
+    deltaFont *= -1;
+  }
+  else if(fontSize < 15)
+  {
+    deltaFont *= -1;
+  }
+
+  fontSize += deltaFont;
+  $("#header").css("font-size",fontSize + "px");
+}, 100)
+
+
 
 });
